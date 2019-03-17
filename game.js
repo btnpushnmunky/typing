b0485a71b25ba9c9f1f3c4f39f490c43f2e8effd
Dan Ross <dan@rosspixelworks.com>
2019-03-16 20:19:54 -0500

    Added score

--- game.js
+++ game.js
@@ -3,19 +3,28 @@
 let wordList = ["word", "typing", "banana", "apple", "boogers"];
 
 const targetWordDiv = document.getElementById("targetWord");
+const scoreSpan = document.getElementById("score");
+let score = 0;
+
+
+function setScore() {
+    scoreSpan.innerHTML = score;    
+}
 
 function setWord() {
     let newWord = wordList[Math.floor(Math.random() * wordList.length)];
     targetWordDiv.innerHTML = newWord;
 }
 
-
 function wordSubmitted() {
     let typedWord = document.getElementById("entryBox").value;
     let targetWord = document.getElementById("targetWord").innerHTML;
     if (typedWord == targetWord) {
         setWord();
+        score += 1;
+        setScore();
         document.getElementById("entryBox").value = "";
+
     }
 }
 
@@ -24,13 +33,14 @@ function focusTextField() {
 }
 
 
-focusTextField();
-setWord();
-
-
 window.onload=function() {
     document.getElementById("typingForm").onsubmit = function () {
         wordSubmitted();
         return false;
     };
 };
+
+//Do some initial setup
+focusTextField();
+setWord();
+scoreSpan.innerHTML = score;
