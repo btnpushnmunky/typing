050316c7082a4990e556efd8b04fd8f95f23fea2
Dan Ross <dan@rosspixelworks.com>
2019-03-16 19:59:08 -0500

    Submitting typedWord, comparing and resetting

--- game.js
+++ game.js
@@ -1,7 +1,36 @@
+/* exported wordSubmitted */
+
+let wordList = ["word", "typing", "banana", "apple", "boogers"];
+
+const targetWordDiv = document.getElementById("targetWord");
+
+function setWord() {
+    let newWord = wordList[Math.floor(Math.random() * wordList.length)];
+    targetWordDiv.innerHTML = newWord;
+}
+
+
 function wordSubmitted() {
     let typedWord = document.getElementById("entryBox").value;
     let targetWord = document.getElementById("targetWord").innerHTML;
     if (typedWord == targetWord) {
-        alert(typedWord);
+        setWord();
+        document.getElementById("entryBox").value = "";
     }
 }
+
+function focusTextField() {
+    document.getElementById("entryBox").focus();
+}
+
+
+focusTextField();
+setWord();
+
+
+window.onload=function() {
+    document.getElementById("typingForm").onsubmit = function () {
+        wordSubmitted();
+        return false;
+    };
+};
