1a6f243563619102620245a7461828a599b0465a
Dan Ross <dan@rosspixelworks.com>
2019-03-16 19:48:32 -0500

    Changing targetWord on correct submission of typedWord without reload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-function wordSubmitted(){
+function wordSubmitted() {
     let typedWord = document.getElementById("entryBox").value;
     let targetWord = document.getElementById("targetWord").innerHTML;
     if (typedWord == targetWord) {
